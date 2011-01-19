5c0a92e5c479a2f806555393a3a30e19da225516
Joshua <jColeChanged@gmail.com>
2011-01-19 12:00:16 -0800

    B

--- src/mysite/core.clj
+++ src/mysite/core.clj
@@ -20,13 +20,21 @@
 
 ;; Models ==================================================================
 (ds/defentity Project [^:key title, url, pitch, details, priority])
+(ds/defentity ExtendableGame [^:key name, url, embed])
 
 (defn add-project-to-store
   [data]
-  (let [project (Project. (:title data) (:url data) (:pitch data)
-			  (:details data) (:priority data))]
-    (ds/save! project)))
+  (ds/save! (Project. (:title data)
+		      (:url data)
+		      (:pitch data)
+		      (:details data)
+		      (:priority data))))
 
+(defn add-extendable-game-to-store
+  [data]
+  (ds/save! (ExtendableGame. (:name data)
+			     (:url data)
+			     (:embed data))))
 ;; Forms ===================================================================
 (defform add-project-form
   :title [required "Title is required."]
@@ -36,6 +44,10 @@
   :priority [required "A priority is required."
 	     an-integer "The priority must be an integer."])
 
+(defform add-extendable-game-form
+  :name [required "Name is required."]
+  :url [required "URL is required."]
+  :embed [required "Must supply text to embed"])
 ;; Templates ===============================================================
 (defn set-input
   [id form]
@@ -46,22 +58,15 @@
   (html/html-content (id (:errors form))))
 
 (html/deftemplate view-homepage-template "mysite/projects/home.html" [] [] [])
-
 (html/deftemplate add-project-template "mysite/projects/add.html"
   [form-data]
   [:div#title :input] (set-input :title form-data)
-  [:div#title :p] (set-error :title form-data)
   [:div#url :input] (set-input :url form-data)
-  [:div#url :p] (set-error :url form-data)
   [:div#pitch :input] (set-input :pitch form-data)
-  [:div#pitch :p] (set-error :pitch form-data)
   [:div#details :textarea] (html/html-content (:details (:values form-data)))
-  [:div#details :p] (set-error :details form-data)
-  [:div#priority :input] (set-input :priority form-data)
-  [:div#priority :p] (set-error :priority form-data))
+  [:div#priority :input] (set-input :priority form-data))
 
 (def *project-selector* [[:.project html/first-child]])
-
 (html/defsnippet project-snippet "mysite/projects/view.html" *project-selector*
   [project]
   [:h2] (html/html-content (.title project))
@@ -69,10 +74,27 @@
   [:p.pitch] (html/html-content (.pitch project))
   [:div.details] (html/html-content (.details project)))
 
+(def *not-free-selector* [:ul#notfree :li])
+(html/defsnippet not-free-snippet "mysite/extendable/home.html" *not-free-selector*
+  [game]
+  [:a] (html/do->
+	(html/content (.name game))
+	(html/set-attr :href (str "/extendable/notfree/?name=" (.name game)))))
+	
+
 (html/deftemplate view-projects-template "mysite/projects/view.html"
   [projects]
   [:.projects] (html/content (map project-snippet projects)))
 
+(html/deftemplate view-extendable-home-template "mysite/extendable/home.html"
+  [games]
+  [:#notfree] (html/content (map not-free-snippet games)))
+
+(html/deftemplate add-extendable-game-template "mysite/extendable/add.html"
+  [game]
+  [:#name] (set-input :name game)
+  [:#url] (set-input :url game)
+  [:#embed] (html/html-content (:embed (:values game))))
 ;; Views ===================================================================
 
 
@@ -94,15 +116,40 @@
   []
   (view-projects-template (ds/query :kind Project)))
 
+(defn add-extendable-game
+  [params]
+  (let [form-data (add-extendable-game-form params)]
+    (if (empty? (:errors form-data))
+      (do
+	(add-extendable-game-to-store (:values form-data))
+	(redirect "/extendable/"))
+      (add-extendable-game-template form-data))))
+(def add-extendable-game-view (require-admin add-extendable-game))
+
+(defn view-extendable-home
+  []
+  (view-extendable-home-template (ds/query :kind ExtendableGame)))
 ;; Routes ==================================================================
-(defroutes admin-routes
+(defroutes extendable-admin-routes
+  (ANY "/extendable/add/" {params :params}
+       (add-extendable-game-view params)))
+
+(defroutes extendable-routes
+  (GET "/extendable/" [] (view-extendable-home))
+  extendable-admin-routes)
+
+(defroutes project-admin-routes
   (GET "/projects/add/" [] (add-project-view {}))
   (POST "/projects/add/" {params :params} (add-project-view params)))
- 
-(defroutes mysite-app-handler
-  admin-routes
-  (GET "/" [] (view-homepage))
+
+(defroutes project-routes
   (GET "/projects/view/" [] (view-projects))
+  project-admin-routes)
+
+(defroutes mysite-app-handler
+  (GET "/" [] (view-homepage))
+  project-routes
+  extendable-routes
   (route/not-found "Page not found."))
 
 (def mysite-app-handler
